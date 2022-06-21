dbd0fcc00071a002cd5fc307155d7b70874f4b1a
carlosgil1 <carlosgil@besoccer.com>
2022-06-21 20:30:26 +0200

    Separate routes in other folder and import to indexjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
-const app = express();
+import indexRoutes from './routes/index.routes'
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
-})
+const app = express();
+app.use(indexRoutes)
 
 app.listen(3000)
 console.log('Server on port', 3000)
